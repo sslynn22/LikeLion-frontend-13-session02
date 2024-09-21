76490ef214afe5732ffd5820e2bf72a2f2189392
HYOBIN <hbin12212@gmail.com>
2024-09-21 13:57:21 +0900

    :memo: add day10

---
+++ day10/mission/App.js
@@ -0,0 +1,13 @@
+//COMPONENTS
+import Header from './components/Header.js';
+import PokemonList from './components/PokemonList.js';
+import PokemonDetail from './components/PokemonDetail.js';
+
+//APIS
+import { getPokemonList, getPokemonDetail } from './modules/api.js';
+
+export default function App($app) {
+    this.state = {
+        // 필요한 state들에는 무엇이 있는지 생각해보세요!
+    };
+}
