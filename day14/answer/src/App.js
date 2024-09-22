584a3e61483281e7791a40b6e182c8cc66c7e70f
HYOBIN <hbin12212@gmail.com>
2024-09-22 12:44:26 +0900

    :memo: add day14

---
+++ day14/answer/src/App.js
@@ -0,0 +1,149 @@
+//COMPONENTS
+import Header from './components/Header.js';
+import PokemonList from './components/PokemonList.js';
+import PokemonDetail from './components/PokemonDetail.js';
+
+//APIS
+import { getPokemonList, getPokemonDetail } from './modules/api.js';
+
+export default function App($app) {
+    const getSearchWord = () => {
+        if (window.location.search.includes('search=')) {
+            return window.location.search.split('search=')[1];
+        }
+        return '';
+    };
+
+    this.state = {
+        type: window.location.pathname.replace('/', ''),
+        pokemonList: [],
+        searchWord: getSearchWord(),
+        currentPage: window.location.pathname,
+    };
+
+    const renderHeader = () => {
+        new Header({
+            $app,
+            initialState: {
+                currentPage: this.state.currentPage,
+                searchWord: this.state.searchWord,
+            },
+            handleClick: async () => {
+                history.pushState(null, null, `/`);
+                const pokemonList = await getPokemonList();
+                this.setState({
+                    ...this.state,
+                    pokemonList: pokemonList,
+                    type: '',
+                    searchWord: getSearchWord(),
+                    currentPage: '/',
+                });
+            },
+            handleSearch: async (searchWord) => {
+                history.pushState(null, null, `?search=${searchWord}`);
+                const searchedPokemonList = await getPokemonList(this.state.type, searchWord);
+                this.setState({
+                    ...this.state,
+                    searchWord: searchWord,
+                    pokemonList: searchedPokemonList,
+                    currentPage: `?search=${searchWord}`,
+                });
+            },
+        });
+    };
+
+    const renderPokemonList = () => {
+        new PokemonList({
+            $app,
+            initialState: this.state.pokemonList,
+            handleItemClick: async (id) => {
+                history.pushState(null, null, `/detail/${id}`);
+                this.setState({
+                    ...this.state,
+                    currentPage: `/detail/${id}`,
+                });
+            },
+            handleTypeClick: async (type) => {
+                history.pushState(null, null, `/${type}`);
+                const pokemonList = await getPokemonList(type);
+                this.setState({
+                    ...this.state,
+                    pokemonList: pokemonList,
+                    searchWord: getSearchWord(),
+                    type: type,
+                    currentPage: `/${type}`,
+                });
+            },
+        });
+    };
+
+    const renderPokemonDetail = async (pokemonId) => {
+        try {
+            const pokemonDetailData = await getPokemonDetail(pokemonId);
+            new PokemonDetail({
+                $app,
+                initialState: pokemonDetailData,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const render = async () => {
+        const path = this.state.currentPage;
+        $app.innerHTML = '';
+        //main
+        if (!path.startsWith('/detail')) {
+            renderHeader();
+            renderPokemonList();
+        }
+        //detail
+        else {
+            const pokemonId = path.split('/detail/')[1];
+            renderHeader();
+            renderPokemonDetail(pokemonId);
+        }
+    };
+
+    this.setState = (newState) => {
+        this.state = newState;
+        render();
+    };
+
+    const init = async () => {
+        const path = this.state.currentPage;
+        //main
+        if (!path.startsWith('/detail')) {
+            try {
+                const initialPokemonList = await getPokemonList(this.state.type, this.state.searchWord);
+                this.setState({
+                    ...this.state,
+                    pokemonList: initialPokemonList,
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        //detail
+        else {
+            render();
+        }
+    };
+
+    window.addEventListener('popstate', async () => {
+        const urlPath = window.location.pathname;
+        const prevType = urlPath.replace('/', '');
+        const prevSearchWord = getSearchWord();
+        const prevPokemonList = await getPokemonList(prevType, prevSearchWord);
+
+        this.setState({
+            ...this.state,
+            type: prevType,
+            pokemonList: prevPokemonList,
+            searchWord: prevSearchWord,
+            currentPage: urlPath,
+        });
+    });
+
+    init();
+}
---
+++ day14/mission/src/App.js
@@ -0,0 +1,57 @@
+//COMPONENTS
+import Header from './components/Header.js';
+import PokemonList from './components/PokemonList.js';
+import PokemonDetail from './components/PokemonDetail.js';
+
+//APIS
+import { getPokemonList, getPokemonDetail } from './modules/api.js';
+
+export default function App($app) {
+    const getSearchWord = () => {
+        if (window.location.search.includes('search=')) {
+            return window.location.search.split('search=')[1];
+        }
+        return '';
+    };
+
+    this.state = {
+        //default 값 알맞게 수정
+        type: '',
+        pokemonList: [],
+        searchWord: getSearchWord(),
+        currentPage: window.location.pathname,
+    };
+
+    // 이번 강의에서 배운 '조건부 렌더링' 적용
+    const renderHeader = () => {
+        // 코드 알맞게 수정
+    };
+
+    const renderPokemonList = () => {
+        // 코드 알맞게 수정
+    };
+
+    const renderPokemonDetail = () => {
+        // 코드 작성
+    };
+
+    const render = async () => {
+        // 코드 작성
+    };
+
+    this.setState = (newState) => {
+        this.state = newState;
+        render();
+    };
+
+    const init = async () => {
+        // 코드 알맞게 수정
+    };
+
+    // 뒤로 가기, 앞으로 가기 기능 완성
+    window.addEventListener('popstate', async () => {
+        // 코드 작성
+    });
+
+    init();
+}
