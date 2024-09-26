0111a61e367d101d1c0eabbac84d84615d5b81f5
HyoBin <hbin12212@gmail.com>
2024-09-26 14:24:46 +0900

    🐛 fix mission

--- day12/mission/src/App.js
+++ day12/mission/src/App.js
@@ -21,7 +21,11 @@ export default function App($app) {
 
     const pokemonList = new PokemonList({
         // 코드 작성
+        //아이템을 클릭하면 "/detail/id" 로 이동할 수 있도록 아래의 함수를 완성하세요.
         handleItemClick: () => {},
+
+        //타입을 클릭하면, 클리한 타입에 해당하는 포켓몬만 띄워지고,
+        // "/type" 으로 이동할 수 있도록 아래의 함수를 완성하세요.
         handleTypeClick: () => {},
     });
 
