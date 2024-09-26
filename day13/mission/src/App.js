e4ef1d8ee8d5ac73978383c716b3fa94e73cd8ff
HyoBin <hbin12212@gmail.com>
2024-09-26 14:30:44 +0900

    🐛 fix mission

--- day13/mission/src/App.js
+++ day13/mission/src/App.js
@@ -24,7 +24,9 @@ export default function App($app) {
         //코드 작성
         $app,
         initialState: {},
+        //'포켓몬 도감'을 클릭하면 "/" 홈으로 돌아갈 수 있도록 함수를 완성하세요.
         handleClick: () => {},
+        //'돋보기 모양'을 누르면 검색 결과를 나타내고, "(기존 url)/?search=searchWord"로 url을 변경하세요.
         handleSearch: () => {},
     });
 
